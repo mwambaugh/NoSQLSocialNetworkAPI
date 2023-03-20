daf2ebe2e904f7c20f4e066c48a5a88a690645e3
Wambaugh <marwambaugh@gmail.com>
2023-03-19 20:07:56 -0700

    workingonSeedsandData

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,22 +1,23 @@
 const router = require('express').Router();
 //from mini project: 
 const {
-  getUsers,
+  getAllUsers,
   getSingleUser,
   createUser,
+  updateUser,
   deleteUser,
-//   addAssignment,
-//   removeAssignment,
+  createFriend,
+  deleteFriend,
 } = require('../../controllers/userController');
 
 //from mini project: /api/students
-router.route('/').get(getUsers).post(createUser);
+router.route('/').get(getAllUsers).post(createUser);
 
 //from mini project: /api/students/:studentId
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).post(updateUser);
 
 //from mini project:// /api/students/:studentId/assignments
-// router.route('/:userId/assignments').post(addAssignment);
+router.route('/:userId/reactions').post(addreactions);
 
 // /api/students/:studentId/assignments/:assignmentId
 // router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
--- controllers/userController.js
+++ controllers/userController.js
@@ -62,6 +62,21 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
+ // Update a thought
+ updateUser(req, res) {
+  User.findOneAndUpdate(
+    { _id: req.params.userId },
+    { $set: req.body },
+    { runValidators: true, new: true }
+  )
+    .then((userData) =>
+      !userData
+        ? res.status(404).json({ message: 'No user with this id!' })
+        : res.json(userData)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+
   // Delete a user and remove them from the thought
   deleteUser(req, res) {
     User.findOneAndRemove({ _id: req.params.userId })
@@ -86,40 +101,39 @@ module.exports = {
         res.status(500).json(err);
       });
   },
-};
 
-//   // Add an assignment to a user
-//   addAssignment(req, res) {
-//     console.log('You are adding an assignment');
-//     console.log(req.body);
-//    User.findOneAndUpdate(
-//       { _id: req.params.userId },
-//       { $addToSet: { assignments: req.body } },
-//       { runValidators: true, new: true }
-//     )
-//       .then((user) =>
-//         !user
-//           ? res
-//               .status(404)
-//               .json({ message: 'No user found with that ID :(' })
-//           : res.json(user)
-//       )
-//       .catch((err) => res.status(500).json(err));
-//   },
-//   // Remove assignment from a user
-//   removeAssignment(req, res) {
-//     User.findOneAndUpdate(
-//       { _id: req.params.userId },
-//       { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
-//       { runValidators: true, new: true }
-//     )
-//       .then((user) =>
-//         !user
-//           ? res
-//               .status(404)
-//               .json({ message: 'No user found with that ID :(' })
-//           : res.json(user)
-//       )
-//       .catch((err) => res.status(500).json(err));
-//   },
-// };
+// Add a reaction to a user
+createFriend(req, res) {
+  console.log('You are adding a friend');
+  console.log(req.body);
+  User.findOneAndUpdate(
+    { _id: req.params.userId },
+    { $addToSet: { friend: req.body } },
+    { runValidators: true, new: true }
+  )
+    .then((userData) =>
+      !userData
+        ? res
+          .status(404)
+          .json({ message: 'No User/Friend found' })
+        : res.json(userData)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+// Remove reaction from a user NEEDS WORK 
+deleteFriend(req, res) {
+  User.findOneAndUpdate(
+    { _id: req.params.userId },
+    { $pull: { friend: { friendId: req.params.userId } } },
+    { runValidators: true, new: true }
+  )
+    .then((UserData) =>
+      !userData
+        ? res
+          .status(404)
+          .json({ message: 'No friend found with that user' })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+  };
