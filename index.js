33fe0d720eb764985c985057884f6a12471240a4
Wambaugh <marwambaugh@gmail.com>
2023-03-19 21:01:52 -0700

    EndofNight

--- index.js
+++ index.js
@@ -7,17 +7,12 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// From the Mini Project 
-// const activity = cwd.includes("01-Activities")
-//   ? cwd.split("/01-Activities/")[1]
-//   : cwd;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server for running on port ${PORT}!`);
   });
 });
--- utils/data.js
+++ utils/data.js
@@ -1,118 +1,109 @@
-const userNames = [
-  'Aaran',
-  'Aaren',
-  'Aarez',
-  'Aarman',
-  'Aaron',
-  'Aaron-James',
-  'Aarron',
-  'Aaryan',
-  'Aaryn',
-  'Aayan',
-  'Aazaan',
-  'Abaan',
-  'Abbas',
-  'Abdallah',
-  'Abdalroof',
-  'Abdihakim',
-  'Abdirahman',
-  'Abdisalam',
-  'Abdul',
-  'Abdul-Aziz',
-  'Abdulbasir',
-  'Abdulkadir',
-  'Abdulkarem',
-  'Smith',
-  'Jones',
-  'Coollastname',
-  'enter_name_here',
-  'Ze',
-  'Zechariah',
-  'Zeek',
-  'Zeeshan',
-  'Zeid',
-  'Zein',
-  'Zen',
-  'Zendel',
-  'Zenith',
-  'Zennon',
-  'Zeph',
-  'Zerah',
-  'Zhen',
-  'Zhi',
-  'Zhong',
-  'Zhuo',
-  'Zi',
-  'Zidane',
-  'Zijie',
-  'Zinedine',
-  'Zion',
-  'Zishan',
-  'Ziya',
-  'Ziyaan',
-  'Zohaib',
-  'Zohair',
-  'Zoubaeir',
-  'Zubair',
-  'Zubayr',
-  'Zuriel',
-  'Xander',
-  'Jared',
-  'Courtney',
-  'Gillian',
-  'Clark',
-  'Jared',
-  'Grace',
-  'Kelsey',
-  'Tamar',
-  'Alex',
-  'Mark',
-  'Tamar',
-  'Farish',
-  'Sarah',
-  'Nathaniel',
-  'Parker',
+// example data
+{
+  "username"; "lernantino",
+  "email"; "lernantino@gmail.com"
+}
+
+// example data
+{
+  "thoughtData"; "Here's a cool thought...",
+  "username"; "lernantino",
+  "userId"; "5edff358a0fcb779aa7b118b"
+}
+
+const username = [
+  'Rigby',
+  'Alice',
+  'Rose',
+  'Emmett',
+  'Donald',
+  'Louie',
+  'Maggie',
+  'Muffy',
+  'Molly',
+  'Bonnie',
+  'Kaiser',
+  'Rhody',
+  'Bowie',
+  'Abbie',
+  'Georgia'
 ];
 
-// const appDescriptions = [
-//   'Decision Tracker',
-//   'Find My Phone',
-//   'Learn Piano',
-//   'Starbase Defender',
-//   'Tower Defense',
-//   'Monopoly Money Manager',
-//   'Movie trailers',
-//   'Hello world',
-//   'Stupid Social Media App',
-//   'Notes',
-//   'Messages',
-//   'Email',
-//   'Compass',
-//   'Firefox',
-//   'Running app',
-//   'Cooking app',
-//   'Poker',
-//   'Deliveries',
-// ];
 
-// // Get a random item given an array
-// const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const thoughtData = [
+  'Decision Trackers are awesome',
+  'Find My Phone is a useful app',
+  'Learn Piano is not very good for learning Piano',
+  'Starbase Defender is a great game, I love it',
+  'Tower Defense is okay',
+  'Monopoly Money is better than real money IMO',
+  'Movie trailers are just the best parts of a movie distilled into 90 seconds',
+  'Hello world, this is a comment',
+  'Social media is a big waste of time',
+  'Notes is my most used app',
+  'Messages is open on my computer 24/7',
+  'Email is open on my computer',
+  'Compass is never opened',
+  'Firefox is great for privacy',
+];
 
-// // Gets a random full name
-// const getRandomName = () =>
-//   `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+const emailData = [
+  'lorem@gmail.com',
+  'imsum@gmail.com',
+  'dolor@gmail.com',
+  'sit@gmail.com',
+  'amet@gmail.com',
+  'consectetur@gmail.com',
+  'adipiscing@gmail.com',
+  'elit@gmail.com',
+  'curabitur@gmail.com',
+  'vel@gmail.com',
+  'hendrerit@gmail.com',
+  'libero@gmail.com',
+  'eleifend@gmail.com',
+  'blandit@gmail.com',
+  'nunc@gmail.com',
+  'ornare@gmail.com',
+  'odio@gmail.com',
+  'ut@gmail.com',
+];
 
-// // Function to generate random assignments that we can add to student object.
-// const getRandomAssignments = (int) => {
-//   const results = [];
-//   for (let i = 0; i < int; i++) {
-//     results.push({
-//       assignmentName: getRandomArrItem(appDescriptions),
-//       score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-//     });
-//   }
-//   return results;
-// };
 
-// // Export the functions for use in seed.js
-// module.exports = { getRandomName, getRandomAssignments };
+const genRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+const getRandomEmail = () => `${emailData[genRandomIndex(emailData)]}`;
+
+const getRandomThought = (words) => {
+  let post = '';
+  for (let i = 0; i < words; i++) {
+    post += ` ${getRandomThought()}`;
+  }
+  return post;
+};
+
+// Get a random item given an array
+const getRandomUserId = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets a random full name
+const getRandomName = () =>
+  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+// Function to generate random comments given a number (ex. 10 comments === getRandomComments(10))
+const getRandomComments = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughtData: getRandomArrItem(comments),
+      username: getRandomName().split(' ')[0],
+    });
+  }
+  return results;
+};
+
+// Export the functions for use in seed.js
+module.exports = {
+  getRandomUsername,
+  getRandomComments,
+  getRandomThoughts,
+  genRandomUserId,
+};
